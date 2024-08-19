db6a1956f2d078268bdabd74c723588943e72600
Dima <babaha3000@gmail.com>
2024-08-19 03:31:10 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use('/api', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ src/routes/auth.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { email, password, name, desiredJobTitle, aboutMe } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      desiredJobTitle,
+      aboutMe,
+    });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: '1m',
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+});
+
+
+router.get("/profile", async (req, res) => {
+  const { email } = req.query;
+  try {
+    if (typeof email === 'string') {
+      const user = await User.findOne({ email });
+      if (user) {
+        res.status(200).json({ name: user.name, aboutMe: user.aboutMe, desiredJobTitle: user.desiredJobTitle });
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    } else {
+      res.status(400).json({ message: 'Invalid email parameter' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+});
+
+export default router;
